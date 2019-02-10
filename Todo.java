e2bdab5b5177a9d32d201458b559ffffc31d9465
Austin Mould <austin.k.mould-1@ou.edu>
2019-02-09 18:20:27 -0600

    Lab 4 with Animal class hopefully finished

---
+++ Todo.java
@@ -0,0 +1,28 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-09-02
+OU ID_______________: 113241045
+OU EMAIL____________: austin.k.mould-1@ou.edu
+
+Tasks:
+1. Finish Animal equals method
+2. Finish Equipment equals method
+3. Devolop AnimalTest class
+4. Develop EquipmentTest class
+5. Develop ZooTest class
+6. Execute TestRunner class
+7. Revise until success
+
+Estimated Time (minutes):
+1. 015
+2. 015
+3. 045
+4. 065
+5. 065
+6. 010
+7. 030
+
+Actual Time (minutes):
+1. 010
+2. XXX
+*/}
